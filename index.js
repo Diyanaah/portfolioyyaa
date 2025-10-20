74b6ec24a5cc4acd8fc0b04bf196d6dcc609a44d
Diyana <shamsudheendiyana@gmail.com>
2025-10-20 12:01:12 +0530

    first11

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var typed= new typed(".text",{
-    Strings:["Frontend Developer","UI/UX Designer"],
+var typed= new Typed(".text",{
+    strings:["Frontend Developer","UI/UX Designer"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
     loop:true
-})
+});
